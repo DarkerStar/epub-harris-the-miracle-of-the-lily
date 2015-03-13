d8025da8ec63288e711a27665c88cc14cd623ce9
Mark A. Gibbs <indi.in.the.wired@gmail.com>
2015-03-13 04:13:27 -0400

    Added Makefile from https://github.com/DarkerStar/epub-build-makefile and fixed minor issues so that book is now buildable

---
+++ book.mk
@@ -0,0 +1,30 @@
+book := harris-the-miracle-of-the-lily
+
+srcdir := src
+
+opf := content.opf
+
+content := toc.xhtml \
+           text/cover.xhtml \
+           text/titlepage.xhtml \
+           text/foreword.xhtml \
+           text/chapter-1.xhtml \
+           text/chapter-2.xhtml \
+           text/chapter-3.xhtml \
+           text/chapter-4.xhtml \
+           text/chapter-5.xhtml \
+           text/chapter-6.xhtml \
+           text/chapter-7.xhtml \
+           text/colophon.xhtml \
+           style/style.css \
+           style/colophon.css \
+           style/cover.css \
+           style/foreword.css \
+           style/index.css \
+           style/titlepage.css \
+           style/toc.css \
+           images/cover.svg \
+           images/cover.png \
+           images/cc-zero.svg \
+           images/publicdomain.svg \
+           toc.ncx
